79eb5200b0303aaef60082e00d16c9060bbda7e2
Nico Tambellini <ntambellini13@gmail.com>
2019-09-09 21:51:33 -0600

    Improved Token Handling with auth0-angular2-jwt

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
  baseURL = 'http://localhost:5000/api/auth/';
+ jwtHelper = new JwtHelperService();
+
 constructor(private http: HttpClient) { }
 
 login(model: any) {
@@ -21,9 +24,14 @@ login(model: any) {
     );
   }
 
-register(model: any) {
-  return this.http.post(this.baseURL + 'register', model);
-}
+  register(model: any) {
+    return this.http.post(this.baseURL + 'register', model);
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
+  }
 }
 
 
